aec16a64a29cbd8367880aa2b6120b9029c375f1
Xiaopow <xiaoyang88@gmail.com>
2015-06-03 16:54:26 +0800

    math first game

--- client/templates/game1.js
+++ client/templates/game1.js
@@ -1,7 +1,16 @@
 Template.game1.helpers({
-	child: function () {
-		var child = Session.get("child");
-		return child;
-		// console.log(child.name)
-	}
-})
+  child: function () {
+    var child = Session.get("child");
+    return child;
+    // console.log(child.name)
+  },
+  question: function () {
+    var question = "1 + 1 = ?"
+    return question;
+  },
+  answers: function () {
+    var answers = [1,2,3,4];
+    return answers
+  }
+
+});
