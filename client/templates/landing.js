255bd8b0928bbda7643a2f6972786e1ff2224299
Fer Martin <fernandomartinsantana@gmail.com>
2015-06-04 22:21:40 +0800

    Small fixes
    - Moved #add-child-btn event into Template events
    - Made buttons for math-game more clickable :)

--- client/templates/landing.js
+++ client/templates/landing.js
@@ -16,20 +16,18 @@ Template.landing.events({
     var name = event.target.childName.value;
     var age = event.target.childAge.value;
 
-    
+
     Meteor.call('addChild', name, age);
-    
+
     event.target.childName.value = '';
     event.target.childAge.value = '';
 
     return false;
 
+  },
+
+  "click #add-child-btn": function(event){
+    $(".new-child").toggle();
   }
 
-});
-
-Template.landing.onRendered(function() {
-  $(document).on('click', '#add-child-btn', function(){
-    $(".new-child").toggle();
-  });
 });
